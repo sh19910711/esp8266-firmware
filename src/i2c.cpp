9b5e12c93ad2c39509a008193dc17216f61c08c6
Hiroyuki Sano <sh19910711@gmail.com>
2016-12-04 13:22:19 +0900

    i2c: remove i2c_init(sda, scl)

--- src/i2c.cpp
+++ src/i2c.cpp
@@ -8,12 +8,6 @@ void i2c_init() {
 }
 
 
-void i2c_init(int sda, int scl) {
-
-    twi_init(sda, scl);
-}
-
-
 void i2c_send(int addr, const void *buf, size_t size) {
 
     twi_writeTo(addr, (unsigned char *) buf, size, true);
